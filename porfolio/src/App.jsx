985bba11bdd25fd0bebcb706432e7b6544bc10b5
lucamaestricolaa <136026451+lucamaestricolaa@users.noreply.github.com>
2023-11-06 09:21:36 -0300

    ..

--- porfolio/src/App.jsx
+++ porfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -8,20 +8,20 @@ import Creations from "./components/Creations";
 import Favorites from "./components/Favorites";
 import Contact from "./components/Contact";
 import { CreationsContextProvider } from "./context/CreationsContext";
-import {FavoritesContextProvider} from "./context/FavoritesContext";
+import { FavoritesContextProvider } from "./context/FavoritesContext";
 
 const App = () => {
   return (
     <Router>
       <div className="app">
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/creations" component={Creations} />
-          <Route path="/favorites" component={Favorites} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/creations" element={<Creations />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
