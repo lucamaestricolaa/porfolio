4ca6fc3a89c8e6609a9313104bbb05864f3fa34b
Gabriel Guivisdalsky <lucamaeskim3@gmail.com>
2023-11-27 08:45:52 -0300

    ..

--- porfolio/src/components/Favorites.jsx
+++ porfolio/src/components/Favorites.jsx
@@ -1,27 +1,9 @@
 import React, { useEffect } from "react";
 import CreationCard from "./CreationCard";
 
-const Favorites = ({onDetailsClick, onAddToFavorites}) => {
-  const [usuario, setUsuario] = React.useState(() => {
-    try {
-      const storedUsuario = localStorage.getItem("usuario");
-      return storedUsuario !== null ? JSON.parse(storedUsuario) : null;
-    } catch (error) {
-      console.error("Error al parsear el usuario:", error);
-      return null;
-    }
-  });
-
-  const [favorites, setFavorites] = React.useState(() => {
-    try {
-      const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
-      return storedFavorites !== null ? JSON.parse(storedFavorites) : null;
-    }
-    catch (error) {
-      console.error("Error al parsear el usuario:", error);
-      return null;
-  }
-});
+const Favorites = ({ onDetailsClick, onAddToFavorites }) => {
+  const [usuario, setUsuario] = React.useState(null);
+  const [favorites, setFavorites] = React.useState([]);
 
   useEffect(() => {
     try {
@@ -43,7 +25,7 @@ const Favorites = ({onDetailsClick, onAddToFavorites}) => {
         setFavorites(parsedFavorites);
       }
     } catch (error) {
-      console.error("Error al parsear el usuario:", error);
+      console.error("Error al parsear los favoritos:", error);
     }
   }, []);
 
@@ -53,20 +35,20 @@ const Favorites = ({onDetailsClick, onAddToFavorites}) => {
 
   const handleDetailsClick = (creation) => {
     onDetailsClick(creation);
-  }
+  };
+
   return (
     <div>
-      <h2>Favoritos</h2>
+      <h2>Favoritos de {usuario}</h2>
       {favorites && favorites.length > 0 ? (
         <div className="creations-list">
           {favorites.map((creation) => (
             <CreationCard
               key={creation.id}
               creation={creation}
-              onDetailsClick={handleDetailsClick}
               onAddToFavorites={handleAddToFavorites}
+              onDetailsClick={handleDetailsClick}
               isFavorite={true}
-              usuario={usuario}
             />
           ))}
         </div>
